d0b7abb2105e94f6e6228cbd82df43f0dc16865c
Luis Arista <luarista.dwfc5@ucamp.utel.edu.mx>
2022-03-16 19:20:26 -0600

    Correcciones en los dialogos

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
  let boton = document.querySelector('.boton_empezar')
  boton.onclick = function () {   
-    let saludo = prompt('Hola Hijo que paso')
+    let saludo = prompt('Hola hijo que paso')
     function mensaje (){
-        if( saludo === saludo.toUpperCase() && saludo !=='ADIOS' && saludo !== 'adios' && saludo !== ""  ){
+        if( saludo === saludo.toUpperCase() && saludo !=='ADIOS TQM' && saludo !== 'ADIÓS TQM' && saludo !== ""  ){
             saludo = prompt('"NO, NO DESDE 1983"')
             mensaje ()
-       } else if( saludo === "" && saludo !== 'adios' ){ 
+       } else if( saludo === ""){ 
             saludo = prompt('HABLA MUCHACHO')
             mensaje()
-       } else if ( saludo !== "" && saludo !== 'adios' && saludo !== 'ADIOS' && saludo !== 'Adios' ) {  
+       } else if ( saludo !== "" && saludo !== 'ADIÓS TQM' && saludo !== 'ADIOS TQM' ) {  
             saludo = prompt('"¡¿EH?! ¡NO TE ESCUCHO, HIJO!"')
             mensaje ()
-       } else if (saludo === 'adios' || saludo === 'Adios' || saludo === 'ADIOS') {
-            let adios = prompt('¿dijiste arroz?')
-            if(adios === 'adios' || adios === 'Adios' || adios === 'ADIOS' ){
-                adios = prompt('¿Mios?')
-                if(adios === 'adios' || adios === 'Adios' || adios === 'ADIOS') {
-                    alert('Ahhhhh adios hijito')
+       } else if (saludo === 'ADIOS TQM' || saludo === 'ADIÓS TQM') {
+            let adios = prompt('¿DIJISTE ARROZ?')
+            if(adios === 'ADIOS TQM' || adios === 'ADIÓS TQM' ){
+                adios = prompt('¿MIOS?')
+                if(adios === 'ADIOS TQM' || adios === 'ADIÓS TQM') {
+                    alert('¡AHHHH! ADIOS HIJITO')
                 }  else {
                     saludo = prompt('"¡¿EH?! ¡NO TE ESCUCHO, HIJO!"')
                     mensaje()
